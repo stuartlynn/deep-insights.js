30861b633dc58b3d12327e5a931f707ef2fc3af6
xavijam <xavijam@gmail.com>
2016-02-17 19:50:55 +0100

    Fixed problem with loading histogram widget

--- src/widgets/histogram/content-view.js
+++ src/widgets/histogram/content-view.js
@@ -127,7 +127,7 @@ module.exports = cdb.core.View.extend({
 
     this.$el.html(
       template({
-        title: this._dataviewModel.get('title'),
+        title: this.model.get('title'),
         showStats: this.model.get('show_stats'),
         itemsCount: !isDataEmpty ? data.length : '-'
       })
